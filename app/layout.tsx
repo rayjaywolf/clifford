3126373165b53be2d9e268ccebe5998ea3329ca3
rayjay8 <jintarehaan@gmail.com>
2025-02-12 16:36:59 +0530

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const fontSans = FontSans({
   variable: '--font-sans'
 })
 
-const title = 'Morphic'
+const title = 'Clifford'
 const description =
   'A fully open-source AI-powered answer engine with a generative UI.'
 
